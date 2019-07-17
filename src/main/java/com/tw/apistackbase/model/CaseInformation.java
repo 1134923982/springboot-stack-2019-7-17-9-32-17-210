ff5f626552fa7a20525888653a636981afd72f3e
1134923982 <1134923982@qq.com>
2019-07-17 21:15:07 +0800

    [Story2 AC1] [implement CaseInformation.java]

---
+++ src/main/java/com/tw/apistackbase/model/CaseInformation.java
@@ -0,0 +1,18 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class CaseInformation {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(nullable = false)
+    private String subjectiveElement;
+    @Column(nullable = false)
+    private String objectiveElement;
+}
