8a552964004944884b77a69ab3da752d1c473ba1
1134923982 <1134923982@qq.com>
2019-07-17 19:29:07 +0800

    [feat][AC1 implement CriminalCase.java and init project]

---
+++ src/main/java/com/tw/apistackbase/model/CriminalCase.java
@@ -0,0 +1,41 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class CriminalCase {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private long time;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
