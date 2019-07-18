43746e4a917a2d1dbe8e9c77673090216319d996
1134923982 <1134923982@qq.com>
2019-07-18 14:11:37 +0800

    [Jayden][implement:Procurator.java]

--- src/main/java/com/tw/apistackbase/model/Procurator.java
+++ src/main/java/com/tw/apistackbase/model/Procurator.java
@@ -3,6 +3,9 @@ package com.tw.apistackbase.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.xml.crypto.Data;
+import java.util.Date;
+
 @Entity
 public class Procurator {
     @Id
@@ -10,6 +13,16 @@ public class Procurator {
     private int id;
 
     private String name;
+
+    private int age;
+
+    private String sex;
+
+    private String idNumber;
+
+    private Date birthday;
+
+    private int experience;
 
     public Procurator(String name) {
         this.name = name;
@@ -29,5 +42,45 @@ public class Procurator {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
     }
 }
